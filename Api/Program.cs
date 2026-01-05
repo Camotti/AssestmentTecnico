b897bf83d746840d39ef29992787fa57431befa0
Camotti <camiropi97@gmail.com>
2026-01-05 15:39:02 -0500

    solving csproj problems for version 10

--- Api/Program.cs
+++ Api/Program.cs
@@ -12,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
